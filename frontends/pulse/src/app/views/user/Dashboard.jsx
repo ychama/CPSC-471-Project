2d0737d65f5f4f6d870a3514ffe6ff76efceffd9
ychama <ychama15@gmail.com>
2021-04-09 22:20:38 -0600

    Add branch list button

--- frontends/pulse/src/app/views/user/Dashboard.jsx
+++ frontends/pulse/src/app/views/user/Dashboard.jsx
@@ -1,8 +1,16 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState, useContext, useRef } from "react";
 import PizzaLogo from "../../MatxLayout/SharedCompoents/PizzaMainLogo";
 import classnames from "classnames";
 import AppContext from "../../appContext";
 import { makeStyles, Grid, Container } from "@material-ui/core";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import Paper from "@material-ui/core/Paper";
+import Popper from "@material-ui/core/Popper";
+import ClickAwayListener from "@material-ui/core/ClickAwayListener";
+import Button from "@material-ui/core/Button";
+import Grow from "@material-ui/core/Grow";
+import axios from "axios";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,18 +42,82 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const useStylesButton = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
+  button: {
+    color: "000000",
+  },
+}));
+
 const Dashboard = () => {
   const { user, authToken } = useContext(AppContext);
-  const [branch, setBranch] = useState("");
+  const [branchList, setBranchList] = useState([]);
+  const [foodItems, setFoodItems] = useState([]);
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef(null);
+  const classButton = useStylesButton();
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event, branchId) => {
+    if (anchorRef.current && anchorRef.current.contains(event.target)) {
+      return;
+    }
+
+    axios
+      .get("http://localhost:8000/restapi/foods/" + branchId + "/", {
+        headers: { Authorization: "Bearer " + authToken },
+      })
+      .then((res) => {
+        console.log(res);
+      });
+
+    setOpen(false);
+  };
+
+  const handleListKeyDown = (event) => {
+    if (event.key === "Tab") {
+      event.preventDefault();
+      setOpen(false);
+    }
+  };
+
+  const prevOpen = React.useRef(open);
+
+  useEffect(() => {
+    if (prevOpen.current === true && open === false) {
+      anchorRef.current.focus();
+    }
+
+    prevOpen.current = open;
+  }, [open]);
 
   useEffect(() => {
     if (user) {
       console.log(user);
       console.log(authToken);
+      //get branches
+      getBranches();
     }
   }, []);
 
-  const getBranches = () => {};
+  const getBranches = async () => {
+    axios
+      .get("http://localhost:8000/restapi/branch/", {
+        headers: { Authorization: "Bearer " + authToken },
+      })
+      .then((res) => {
+        console.log(res.data);
+        setBranchList(res.data);
+      });
+  };
 
   const classes = useStyles();
   return (
@@ -66,6 +138,54 @@ const Dashboard = () => {
             className={classnames(classes.gridCell, classes.gridRowOne)}
           >
             <PizzaLogo width={200} />
+            <Button
+              ref={anchorRef}
+              aria-controls={open ? "menu-list-grow" : undefined}
+              aria-haspopup="true"
+              onClick={handleToggle}
+              style={{ color: "blue" }}
+            >
+              Branch List Selection
+            </Button>
+            <Popper
+              open={open}
+              anchorEl={anchorRef.current}
+              role={undefined}
+              transition
+              disablePortal
+            >
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  style={{
+                    transformOrigin:
+                      placement === "bottom" ? "center top" : "center bottom",
+                  }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={handleClose}>
+                      <MenuList
+                        autoFocusItem={open}
+                        id="menu-list-grow"
+                        onKeyDown={handleListKeyDown}
+                      >
+                        {branchList.map((branch, key) => {
+                          return (
+                            <MenuItem
+                              onClick={(event) => {
+                                handleClose(event, branch.branch_id);
+                              }}
+                            >
+                              {branch.branch_id}
+                            </MenuItem>
+                          );
+                        })}
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
           </Grid>
         </Grid>
       </Container>
